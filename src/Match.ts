410ffeb5a0dd7ca7622afc75b12b1696009b0ba6
Indumuni Abeyaratna <indumuni@gmail.com>
2025-02-15 01:26:05 +1100

    Setup player so that we don't have to hardcode everytime in test

--- src/Match.ts
+++ src/Match.ts
@@ -1,6 +1,22 @@
 class Match {
+  private playerOne: string;
+  private playerTwo: string;
+  constructor(player1: string, player2: string) {
+    this.playerOne = player1;
+    this.playerTwo = player2;
+
+  }
+
   score(): string {
     return `0-0`;
+  }
+
+  player1() {
+    return this.playerOne;
+  }
+
+  player2() {
+    return this.playerTwo;
   }
 }
 
--- test/Match.test.ts
+++ test/Match.test.ts
@@ -1,7 +1,28 @@
 import Match from "../src/Match";
 
+describe("Match players", () => {
+  let match: Match;
+  beforeAll(() => {
+      match = new Match("Player X", "Player Y");
+    }
+  );
+  test("Player1 should return first match player", () => {
+    expect(match.player1()).toEqual("Player X");
+  })
+  test("Player2 should return second match player", () => {
+    expect(match.player2()).toEqual("Player Y");
+  })
+})
+
+
 describe("Match score suite", () => {
+  let match: Match;
+  beforeAll(() => {
+      match = new Match("Player 1", "Player 2");
+    }
+  );
+
   test("Should show initial score at the start", () => {
-    expect(new Match().score()).toEqual("0-0");
+    expect(match.score()).toEqual("0-0");
   });
 });
