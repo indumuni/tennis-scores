a47b897e2c3b28d39579128566331e16ca1dcaea
Indumuni Abeyaratna <indumuni@gmail.com>
2025-02-17 14:45:27 +1100

    Increment score instead of pointWonBy since that gives a different meaning

--- src/Scorable.ts
+++ src/Scorable.ts
@@ -13,11 +13,6 @@ abstract class Scorable {
     return `${this.displayScore(PLAYER_1)}-${this.displayScore(PLAYER_2)}`
   }
 
-  // public pointWonBy(player: PLAYER) {
-  //   let score = this.getScore(player)
-  //   this.setScore(player, ++score)
-  // }
-
   public incScore(player: PLAYER) {
     let score = this.getScore(player)
     this.setScore(player, ++score)
